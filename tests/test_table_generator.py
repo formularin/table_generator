e29d39f3d74e4249d943412fa49ed09a18f3dd8d
lol-cubes <arinmkhare@gmail.com>
2019-07-18 23:56:09 -0400

    packaged and uploaded to testpypi

--- tests/test_table_generator.py
+++ tests/test_table_generator.py
@@ -1,6 +1,12 @@
+import os
+import sys
 import unittest
-import table_generator
-import subprocess
+
+package_dir = '/'.join(os.path.dirname(__file__).split('/')[:-1])
+if package_dir not in sys.path:
+    sys.path.append(package_dir)
+
+from table_generator import generate_html
 
 def get_data(file):
     
@@ -15,24 +21,23 @@ class TestTableGenerator(unittest.TestCase):
     def test_generate_html(self):
         
         # obtain test output
-        csv_data = get_data('test.csv')
-        test_output = table_generator.Table(csv_data).generate_html(True, True)
+        csv_data = get_data(f'{os.getcwd()}/tests/test.csv')
+        test_output = generate_html(csv_data, True, True, False)
         
         # obtain accurate output
-        with open('table.html', 'r') as f:
+        with open(f'{os.getcwd()}/tests/table.html', 'r') as f:
             html_output = f.read()
 
         self.assertEqual(test_output, html_output)
-        
-    def test_args(self):
-        
-        real_file = 'test.csv'
-        fake_file = 'data.csv'
 
-        try:
-            subprocess.call(["./table_generator.sh", real_file, '-h', '-c'])
-        except subprocess.CalledProcessError as e:
-            print(e)
+        pretty_test_output = generate_html(csv_data, True, True, True)
+
+        with open(f'{os.getcwd()}/tests/pretty_table.html', 'r') as f:
+            pretty_html_output = f.read()
+
+        for real, test in zip(
+            *[i.split('\n') for i in [pretty_html_output, pretty_test_output]]):
+            self.assertEqual(test, real)
 
 
 if __name__ == "__main__":
